53086d3b742ac2f21858c08f22f5946a6825a659
Erickson <esernero@outlook.com>
2023-07-20 01:37:17 +0800

    searching for bug

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/React">
       <Provider store={store}>
         <App />
       </Provider>
